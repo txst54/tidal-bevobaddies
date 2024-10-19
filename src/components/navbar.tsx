9833af4e82808249c260c52473a5a0fb6c4aa07f
Jiwon Kim <jiwon05@Andys-MacBook-Air-4.local>
2024-10-19 18:51:43 -0500

    bruh

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { MagnifyingGlass, User, Bell} from '@phosphor-icons/react';
+import { MagnifyingGlass, User, Bell } from '@phosphor-icons/react';
 
 function NavBar() {
     return (
         <div className="flex flex-col justify-center h-16 p-4 bg-black border-b border-zinc-800">
-            <div className="flex flex-row justify-between">
-                <div className="flex flex-row h-16">
-                    <MagnifyingGlass size={24} className="text-white absolute left-50 top-5 z-10" />
-                    <input className="pl-10 pb-2 pt-3 font-urbanist bg-black text-white focus:border-0 focus:outline-none" placeholder="Search" />
+            <div className="flex flex-row justify-between items-center">
+                <div className="flex flex-row items-center">
+                    <MagnifyingGlass size={24} className="text-white mr-2" />
+                    <input 
+                        className="pl-2 pb-2 pt-3 font-urbanist bg-black text-white focus:border-0 focus:outline-none" 
+                        placeholder="Search" 
+                    />
                 </div>
                 <div className="flex flex-row items-center">
                     <div className="font-urbanist p-6 hover:cursor-pointer hover:text-zinc-300 text-white transition-all duration-200">
@@ -19,7 +22,7 @@ function NavBar() {
                     <div className="px-6 pr-10">
                         <Bell size={24} className='text-white hover:cursor-pointer hover:text-zinc-300'/>
                     </div>
-                    <div className="px-6 bg-zinc-900 rounded-full h-10 flex flex-col justify-center hover:cursor-pointer hover:text-zinc-300'">
+                    <div className="px-6 bg-zinc-900 rounded-full h-10 flex flex-col justify-center hover:cursor-pointer hover:text-zinc-300">
                         <User size={24} className='text-white'/>
                     </div>
                 </div>
