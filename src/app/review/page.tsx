3c38bf05f7688ef13b272f88be5dcca2c893a2b1
Jiwon Kim <jiwon05@Andys-MacBook-Air-4.local>
2024-10-19 18:45:50 -0500

    homenavbar

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -2,6 +2,20 @@
 import NavBar from "@/components/navbar";
 import SideBar from "@/components/sidebar";
 
+const DashboardTh = ({ title, dir, bool }: { title: string; dir?: string; bool?: boolean }) => {
+    return (
+        <th
+            className={`py-3 px-6 text-left ${bool ? "font-semibold" : "font-medium"} border-zinc-800
+      ${dir === "left" ? "border-t border-l border-b" :
+                dir === "right" ? "border-t border-r border-b" :
+                    "border-y"} 
+      border-collapse`}
+        >
+            {title}
+        </th>
+    );
+};
+
 function reviewPage() {
     return (
         <div className="flex flex-row w-full h-screen bg-black">
@@ -9,14 +23,14 @@ function reviewPage() {
             <div className="flex flex-col w-full">
                 <NavBar />
                 <div className="flex-grow p-4">
-                    <table className="min-w-full border-collapse rounded-lg overflow-hidden">
+                    <table className="min-w-full border-collapse rounded-lg overflow-hidden shadow-lg">
                         <thead>
-                            <tr className="bg-black text-white">
-                                <th className="border border-gray-600 p-4 text-left">Requirements</th>
-                                <th className="border border-gray-600 p-4 text-left">Proofs</th>
+                            <tr className="bg-zinc-900 text-zinc-400 uppercase text-xs">
+                                <DashboardTh title={"Requirements"} dir={"left"} bool />
+                                <DashboardTh title={"Proofs"} dir={"left"} bool />
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody className="text-zinc-400 text-sm font-light">
                             {[
                                 {
                                     requirement: "Proof of shipping",
@@ -43,19 +57,19 @@ function reviewPage() {
                                     proof: ""
                                 }
                             ].map((item, index) => (
-                                <tr key={index} className={`border-b ${index % 2 === 0 ? 'bg-gray-800' : 'bg-black'} text-white hover:bg-gray-700`}>
-                                    <td className="border border-gray-600 p-4">{item.requirement}</td>
-                                    <td className="border border-gray-600 p-4">{item.proof}</td>
+                                <tr key={index} className={`border-b border-zinc-800 transition-all duration-200 hover:bg-zinc-800`}>
+                                    <td className="py-4 px-6 text-zinc-300">{item.requirement}</td>
+                                    <td className="py-4 px-6 text-zinc-300">{item.proof}</td>
                                 </tr>
                             ))}
                         </tbody>
                     </table>
 
                     <div className="flex justify-end space-x-4 mt-4"> {/* Added margin top for spacing */}
-                        <button className="bg-green-700 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-200">
+                        <button className="bg-zinc-600 text-white px-4 py-2 rounded hover:bg-zinc-500 transition duration-200">
                             Confirm
                         </button>
-                        <button className="bg-red-700 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-200">
+                        <button className="bg-zinc-600 text-white px-4 py-2 rounded hover:bg-zinc-500 transition duration-200">
                             Deny
                         </button>
                     </div>
